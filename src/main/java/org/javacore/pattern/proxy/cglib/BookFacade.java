7a755a40aafb2dc3ded5a4f1da5ce7bbf1d24775
jackznq <1577237702@qq.com>
2017-09-05 09:41:48 +0800

    cglib动态代理

---
+++ src/main/java/org/javacore/pattern/proxy/cglib/BookFacade.java
@@ -0,0 +1,8 @@
+package org.javacore.pattern.proxy.cglib;
+
+/**
+ *  要代理类的接口
+ */
+public interface BookFacade {
+    void addBook();
+}
---
+++ src/main/java/org/javacore/pattern/proxy/cglib/TestCglib.java
@@ -0,0 +1,10 @@
+package org.javacore.pattern.proxy.cglib;
+
+public class TestCglib {
+
+    public static void main(String[] args) {
+        BookFacadeCglib cglib=new BookFacadeCglib();
+        BookFacadeImpl1 bookCglib=(BookFacadeImpl1)cglib.getInstance(new BookFacadeImpl1());
+        bookCglib.addBook();
+    }
+}
