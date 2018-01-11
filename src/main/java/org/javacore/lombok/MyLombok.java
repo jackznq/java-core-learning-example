27dfe6a221e4b91bbde2d7fb34dbef8d334bcd46
jackznq <1577237702@qq.com>
2018-01-11 13:51:04 +0800

    lombok测试

---
+++ src/main/java/org/javacore/lombok/MyLombok.java
@@ -0,0 +1,12 @@
+package org.javacore.lombok;
+
+/**
+ * Created by ddfhznq on 2017/12/26.
+ */
+public class MyLombok {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setCertNo("er");
+        System.out.println(student);
+    }
+}
