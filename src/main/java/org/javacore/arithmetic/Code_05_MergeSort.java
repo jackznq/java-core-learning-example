045ccf78bddeeade6a2ae13aeda28157b8a63860
shzhangnaiquan <shzhangnaiquan@jd.com>
2019-03-15 11:16:18 +0800

    归并排序自己实现

--- src/main/java/org/javacore/arithmetic/Code_05_MergeSort.java
+++ src/main/java/org/javacore/arithmetic/Code_05_MergeSort.java
@@ -118,9 +118,48 @@ public class Code_05_MergeSort {
 
         int[] arr = generateRandomArray(maxSize, maxValue);
         printArray(arr);
-        mergeSort(arr);
+        myMergeSort(arr);
         printArray(arr);
 
     }
 
+
+    public static void myMergeSort(int arr[]) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        myMergeSort(arr, 0, arr.length - 1);
+    }
+
+    private static void myMergeSort(int[] arr, int l, int r) {
+
+        if (l == r) {
+            return;
+        }
+
+        int mid = (l + r) / 2;
+        myMergeSort(arr, l, mid);
+        myMergeSort(arr, mid + 1, r);
+        myMergeProcess(arr, l, mid, r);
+    }
+
+    private static void myMergeProcess(int[] arr, int l, int mid, int r) {
+        int[] help = new int[r - l + 1];
+        int a = 0;
+        int p1 = l;
+        int p2 = mid + 1;
+        while (p1 <= mid && p2 <= r) {
+            help[a++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+        }
+        while (p1 <= mid) {
+            help[a++] = arr[p1++];
+        }
+        while (p2 <= r) {
+            help[a++] = arr[p2++];
+        }
+        for (a = 0; a < help.length; a++) {
+            arr[l + a] = help[a];
+        }
+
+    }
 }
