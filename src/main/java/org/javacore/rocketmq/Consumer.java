e759c852f325921355db57a61dc9e3d2ab7f61bc
shzhangnaiquan <shzhangnaiquan@jd.com>
2019-03-16 22:58:13 +0800

    rocketmq

--- src/main/java/org/javacore/rocketmq/Consumer.java
+++ src/main/java/org/javacore/rocketmq/Consumer.java
@@ -1,61 +1,101 @@
-//package org.javacore.rocketmq;
-//
-//import java.util.List;
-//
-//import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
-//import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
-//import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
-//import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
-//import com.alibaba.rocketmq.client.exception.MQClientException;
-//import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
-//import com.alibaba.rocketmq.common.message.MessageExt;
-//
-//public class Consumer {
-//	public static void main(String[] args) throws MQClientException {
-//
-//		//声明并初始化一个consumer
-//	     //需要一个consumer group名字作为构造方法的参数，这里为consumer1
-//	     DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("consumer1");
-//
-//	     //同样也要设置NameServer地址
-//	     consumer.setNamesrvAddr("192.168.140.128:9876;192.168.140.129:9876");
-//
-//	     //这里设置的是一个consumer的消费策略
-//	     //CONSUME_FROM_LAST_OFFSET 默认策略，从该队列最尾开始消费，即跳过历史消息
-//	     //CONSUME_FROM_FIRST_OFFSET 从队列最开始开始消费，即历史消息（还储存在broker的）全部消费一遍
-//	     //CONSUME_FROM_TIMESTAMP 从某个时间点开始消费，和setConsumeTimestamp()配合使用，默认是半个小时以前
-//	     consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
-//
-//	     //设置consumer所订阅的Topic和Tag，*代表全部的Tag
-//	     consumer.subscribe("TopicTest", "*");
-//
-//	     //设置一个Listener，主要进行消息的逻辑处理
-//	     consumer.registerMessageListener(new MessageListenerConcurrently() {
-//
-//	         @Override
-//	         public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
-//	                                                         ConsumeConcurrentlyContext context) {
-//
-//	            // System.out.println(Thread.currentThread().getName() + " Receive New Messages: " + msgs);
-//	        	 for (MessageExt messageExt : msgs) {
-//
-//              //       System.out.println("messageExt: " + messageExt);//输出消息内容
-//                    String messageBody = new String(messageExt.getBody());
-//         //            System.out.println(messageExt.getStoreHost());//获取对应rocketmq服务器ip地址
-//	        		 System.out.println("消费响应：msgId : " + messageExt.getMsgId() + ",  msgBody : " + messageBody);//输出消息内容
-//                 }
-//
-//	             //返回消费状态
-//	             //CONSUME_SUCCESS 消费成功
-//	             //RECONSUME_LATER 消费失败，需要稍后重新消费
-//	             return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
-//	         }
-//	     });
-//
-//	     //调用start()方法启动consumer
-//	     consumer.start();
-//
-//	     System.out.println("Consumer Started.");
-//	 }
-//
-//}
+package org.javacore.rocketmq;
+
+import com.alibaba.fastjson.JSON;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.consumer.ConsumeFromWhere;
+import org.apache.rocketmq.common.message.MessageExt;
+
+import java.util.List;
+
+/**
+ * @author dddd
+ * @description 消息消费者
+ * @date 2016-04-07
+ */
+public class Consumer {
+
+    /*
+     * Constructs a client instance with your account for accessing DefaultMQConsumer
+     */
+    private static DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("ConsumerGroupName");
+    private static int initialState = 0;
+
+    private Consumer() {
+
+    }
+
+    public static DefaultMQPushConsumer getDefaultMQPushConsumer() {
+        if (consumer == null) {
+            consumer = new DefaultMQPushConsumer("ConsumerGroupName");
+        }
+
+        if (initialState == 0) {
+            consumer.setNamesrvAddr("47.244.128.241:9876");
+            consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+            initialState = 1;
+        }
+
+        return consumer;
+    }
+
+    public static void receiveMsg() {
+
+        // 获取消息生产者
+        DefaultMQPushConsumer consumer = Consumer.getDefaultMQPushConsumer();
+
+        // 订阅主体
+        try {
+            consumer.subscribe("TopicTest1", "*");
+
+            consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+                /**
+                 * * 默认msgs里唯独一条消息，能够通过设置consumeMessageBatchMaxSize參数来批量接收消息
+                 */
+                public ConsumeConcurrentlyStatus consumeMessage(
+                    List<MessageExt> msgs, ConsumeConcurrentlyContext context) {
+
+
+                    MessageExt msg = msgs.get(0);
+
+                    if (msg.getTopic().equals("TopicTest1")) {
+                        // 运行TopicTest1的消费逻辑
+                        if (msg.getTags() != null && msg.getTags().equals("TagA")) {
+                            // 运行TagA的消费
+                            System.out.println("MsgBody:{}" + JSON.toJSONString(msg.getBody()));
+
+                        } else if (msg.getTags() != null
+                            && msg.getTags().equals("TagC")) {
+                            // 运行TagC的消费
+                        } else if (msg.getTags() != null
+                            && msg.getTags().equals("TagD")) {
+                            // 运行TagD的消费
+                        }
+                    } else if (msg.getTopic().equals("TopicTest2")) {
+                        // 运行TopicTest2的消费逻辑
+                    }
+
+                    return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                }
+            });
+
+            /**
+             * Consumer对象在使用之前必须要调用start初始化。初始化一次就可以<br>
+             */
+            consumer.start();
+
+        } catch (MQClientException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        receiveMsg();
+    }
+
+}
