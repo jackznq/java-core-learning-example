49326f7a5b79514f8b970736cba0478db2e89172
shzhangnaiquan <shzhangnaiquan@jd.com>
2019-03-18 11:04:29 +0800

    几种循环遍历效率

--- src/main/java/org/javacore/collection/list/ArrayListT.java
+++ src/main/java/org/javacore/collection/list/ArrayListT.java
@@ -1,56 +1,109 @@
 package org.javacore.collection.list;
 
-import java.math.BigDecimal;
-import java.text.NumberFormat;/*
- * Copyright [2015] [Jeff Lee]
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * @author Jeff Lee
  * @since 2015-6-18 21:05:38
- * 	ArrayList的使用
+ * ArrayList的使用
  */
-public class ArrayListT{
+public class ArrayListT {
 
-	public static void main(String[] args){
-		listMethods();
-	}
+    private static final int COUNT = 10000;
+    private static List<Integer> persons = new ArrayList<>();
 
-	/**
-	 * 测试ArrayList基本操作方法，其角标的使用及迭代器next()的使用。
-	 */
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	public static void listMethods(){
-//		List a1 = new ArrayList<String>();
-//		a1.add("List01");
-//		a1.add("List03");
-//		a1.add("List04");
-//        System.out.println(a1.contains("List01"));
-//		System.out.print("原来集合：\n\t"+a1+"\n");
-//		a1.add(1,"List02");
-//		System.out.print("指定角标1插入：\n\t"+a1+"\n");
-//		a1.remove(2);
-//		System.out.print("指定角标2删除：\n\t"+a1+"\n");
-//		System.out.print("指定角标2查询：\n\t"+a1.get(2)+"\n");
-//
-//		Iterator i1 = a1.iterator();
-//		System.out.println("用迭代器查询全部元素：");
-//		while (i1.hasNext())
-//			System.out.print(i1.next()+"\t");
-        BigDecimal bigDecimal = BigDecimal.valueOf(100);
-        String s =NumberFormat.getCurrencyInstance().format( bigDecimal);
-        System.out.println(s);
+    public static void main(String[] args) {
+        init();
+        System.out.println("Iterator迭代遍历的消耗时间为:" + testIterator(persons) + "s");
+        System.out.println("ForEach遍历的消耗时间为:" + testForEach(persons) + "s");
+        System.out.println("For循环遍历的消耗时间为:" + testFor(persons) + "s");
     }
+
+    public static void init() {
+        //初始化，生成对象个数
+        for (int i = 0; i < COUNT; i++) {
+            persons.add(i);
+        }
+    }
+
+
+    /**
+     * 测试ArrayList基本操作方法，其角标的使用及迭代器next()的使用。
+     */
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public static void listMethods() {
+
+        List<Integer> integers = new ArrayList<>(100000);
+        for (int a = 0; a < 100000; a++) {
+            int i = (int) (Math.random() * 100);
+            integers.add(i);
+        }
+
+        //foreach
+        Long startForEach = System.currentTimeMillis();
+        for (int i : integers) {
+            System.out.println(i);
+        }
+        Long endForEach = System.currentTimeMillis();
+
+        System.out.println("ForEach耗时" + (endForEach - startForEach));
+
+        int size = integers.size();
+        Long startIterator = System.currentTimeMillis();
+        for (Iterator<Integer> iterator = integers.iterator(); iterator.hasNext(); ) {
+            System.out.println(iterator.next());
+        }
+        Long endIterator = System.currentTimeMillis();
+
+        System.out.println("Iterator耗时" + (endIterator - startIterator));
+
+        Long startGeneralFor = System.currentTimeMillis();
+
+        for (int i = 0; i < size; i++) {
+            System.out.println(integers.get(i));
+        }
+        Long endGeneralFor = System.currentTimeMillis();
+        System.out.println("GeneralFor耗时" + (endGeneralFor - startGeneralFor));
+    }
+
+    //Iterator遍历
+    private static long testIterator(List<Integer> integers) {
+        //开始编译执行时间
+        long start = System.currentTimeMillis();
+        for (Iterator<Integer> iterator = integers.iterator(); iterator.hasNext(); ) {
+            iterator.next();
+
+        }
+        //执行完后的时间
+        long end = System.currentTimeMillis();
+        return (end - start);
+    }
+
+    //foEach循环遍历
+    private static long testForEach(List<Integer> integers) {
+        //开始编译执行时间
+        long start = System.currentTimeMillis();
+        for (Integer p : integers) {
+
+        }
+        //执行完后的时间
+        long end = System.currentTimeMillis();
+        return (end - start);
+    }
+
+    //for循环遍历
+    private static long testFor(List<Integer> integers) {
+        //开始编译执行时间
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < integers.size(); i++) {
+            integers.get(i);
+        }
+        //执行完后的时间
+        long end = System.currentTimeMillis();
+        return end - start;
+
+    }
+
 }
