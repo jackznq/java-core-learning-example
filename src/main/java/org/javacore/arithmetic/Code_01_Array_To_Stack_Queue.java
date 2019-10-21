dbab555417a11eb8dfff1c62e7d39f2851e7b134
jackznq <zhangnaiquan@qjdchina.com>
2019-10-21 08:56:45 +0800

    commit

--- src/main/java/org/javacore/arithmetic/Code_01_Array_To_Stack_Queue.java
+++ src/main/java/org/javacore/arithmetic/Code_01_Array_To_Stack_Queue.java
@@ -1,6 +1,7 @@
 package org.javacore.arithmetic;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Code_01_Array_To_Stack_Queue {
 
@@ -37,52 +38,50 @@ public class Code_01_Array_To_Stack_Queue {
         }
     }
 
-    public static class ArrayQueue {
-        private Integer[] arr;
-        private Integer size;
-        private Integer first;
-        private Integer last;
+    /**
+     * 数组队列
+     */
+    public static class ArrayQueue<E> {
+
+        private List<E> list;
+
 
         public ArrayQueue(int initSize) {
-            if (initSize < 0) {
-                throw new IllegalArgumentException("The init size is less than 0");
-            }
-            arr = new Integer[initSize];
-            size = 0;
-            first = 0;
-            last = 0;
+            list = new ArrayList<>(initSize);
+        }
+
+        public ArrayQueue() {
+            list = new ArrayList<>();
         }
 
         public Integer peek() {
-            if (size == 0) {
-                return null;
-            }
-            return arr[first];
+            return 1;
         }
 
-        public void push(int obj) {
-            if (size == arr.length) {
-                throw new ArrayIndexOutOfBoundsException("The queue is full");
-            }
-            size++;
-            arr[last] = obj;
-            last = last == arr.length - 1 ? 0 : last + 1;
+        public void enqueue(E e) {
+            list.add(e);
         }
 
-        public Integer poll() {
-            if (size == 0) {
-                throw new ArrayIndexOutOfBoundsException("The queue is empty");
-            }
-            size--;
-            int tmp = first;
-            first = first == arr.length - 1 ? 0 : first + 1;
-            return arr[tmp];
+        public void getFront() {
+
+        }
+
+        public E dequeue() {
+            return null;
+        }
+
+        public int getSize() {
+            return list.size();
+        }
+
+        public int getCapacity() {
+            return 0;
         }
     }
 
     public static void main(String[] args) {
 
-        String isValid = "([)]";
+        String isValid = "(]";
         System.out.println(isValid(isValid));
     }
 
