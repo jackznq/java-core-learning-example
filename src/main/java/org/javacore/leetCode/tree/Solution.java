51b79d964eb25411c0f085ce33802ac4d5ec6331
jackznq <zhangnaiquan@qjdchina.com>
2019-12-24 17:15:50 +0800

    test

--- src/main/java/org/javacore/leetCode/tree/Solution.java
+++ src/main/java/org/javacore/leetCode/tree/Solution.java
@@ -1,6 +1,10 @@
 package org.javacore.leetCode.tree;
 
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 class Solution {
 
     public static void main(String[] args) {
@@ -12,29 +16,27 @@ class Solution {
 //        bst.addForeach(2);
 //        bst.addForeach(5);
 //        bst.addForeach(7);
-        bst.addRecur(3);
-        bst.addRecur(6);
-        bst.addRecur(1);
-        bst.addRecur(2);
-        bst.addRecur(5);
-        bst.addRecur(7);
+        Random random = new Random();
+        for (int i = 0; i < 100; i++) {
+            bst.addRecur(random.nextInt(10000));
+        }
+
+        List<Integer> result = new ArrayList<>();
+        while (!bst.isEmpty()) {
+            result.add((Integer) bst.removeMin().e);
+        }
+        System.out.println(result);
 //        System.out.println(bst);
 //        bst.preOrderForeach();
 //        System.out.println("----");
 //        bst.inOrderTraversal();
 //        System.out.println("----");
 //        bst.levelOrder();
-        System.out.println("-------------------------");
-        System.out.println(bst.removeMin());
-        System.out.println(bst.toString());
-        System.out.println("-------------------------");
-
-        System.out.println(bst.removeMax());
-        System.out.println(bst.toString());
-        System.out.println("-------------------------");
-
-        System.out.println(bst.removeMax());
-        System.out.println(bst.toString());
+//        System.out.println(bst.removeMin());
+//        System.out.println(bst.toString());
+//
+//        System.out.println(bst.removeMax());
+//        System.out.println(bst.toString());
     }
 
 
