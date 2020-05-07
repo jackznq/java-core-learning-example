7161d0ee57e5e1c6f737ada2d989ffa0c1e3cb63
jackznq <zhangnaiquan@qjdchina.com>
2020-05-07 17:43:43 +0800

    dp

--- src/main/java/org/javacore/leetCode/dp/DynamicProgramming.java
+++ src/main/java/org/javacore/leetCode/dp/DynamicProgramming.java
@@ -23,4 +23,44 @@ public class DynamicProgramming {
         }
         return sell;
     }
+
+    public static int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0] == null || grid[0].length == 0) {
+            return 0;
+        }
+        int row = grid.length;
+        int col = grid[0].length;
+        int[][] res = new int[row][col];
+        res[0][0] = grid[0][0];
+        for (int i = 1; i < row; i++) {
+            res[i][0] = res[i - 1][0] + grid[i][0];
+        }
+        for (int j = 1; j < col; j++) {
+            res[0][j] = res[0][j - 1] + grid[0][j];
+        }
+        for (int i = 1; i < row; i++) {
+            for (int j = 1; j < col; j++) {
+                res[i][j] = Math.min(res[i - 1][j], res[i][j - 1]) + grid[i][j];
+            }
+        }
+        return res[row - 1][col - 1];
+    }
+
+    public int uniquePaths(int m, int n) {
+        int[][] res = new int[m][n];
+        for (int i = 0; i < res.length; i++) {
+            res[i][0] = 1;
+        }
+
+        for (int i = 0; i < res[0].length; i++) {
+            res[0][i] = 1;
+        }
+
+        for (int i = 1; i < res.length; i++) {
+            for (int j = 1; j < res[0].length; j++) {
+                res[i][j] = res[i - 1][j] + res[i][j - 1];
+            }
+        }
+        return res[m - 1][n - 1];
+    }
 }
